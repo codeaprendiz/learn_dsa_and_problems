5912114f337456efdeb5a34d89eddf90c1e07ce9
codeaprendiz <codeaprendiz@gmail.com>
2023-12-02 00:37:28 +0400

    chore: update

--- home/codility/easy/taskset_easy_codility/task_001_binary_gap/binary_gap.go
+++ home/codility/easy/taskset_easy_codility/task_001_binary_gap/binary_gap.go
@@ -10,19 +10,26 @@ func Solution(N int) int {
 	binaryN := strconv.FormatInt(int64(N), 2)
 	fmt.Println(binaryN)
 
-	currentGap, maxGap := -1, 0
+	currentGap, maxGap := 0, 0
 	// Iterate through each character in the binary string
 	for _, bit := range binaryN {
-		if bit == 1 {
+		fmt.Printf("currentGap : %v, maxGap : %v, bit : %v\n", currentGap, maxGap, bit)
+		fmt.Println(bit)
+		if bit == '1' {
+			fmt.Println("Bit is 1")
 			// Update maxGap if currentGap is larger
 			if currentGap > maxGap {
+				fmt.Println("currentGap > maxGap")
 				maxGap = currentGap
 			}
 			// Reset curentGap to 0 after finding bit 1
 			currentGap = 0
-		} else if currentGap >= 0 {
+		} else if bit == '0' {
 			// Only count gaps after the first one has been found
+			fmt.Println("currentGap >= 0 OR bit is 0")
 			currentGap += 1
+		} else {
+			fmt.Println("Default")
 		}
 	}
 
@@ -30,5 +37,5 @@ func Solution(N int) int {
 }
 
 func main() {
-	Solution(1041)
+	fmt.Println(Solution(1041))
 }
