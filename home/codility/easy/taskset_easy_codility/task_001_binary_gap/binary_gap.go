7cd8835990d356662d5cc983da7f447c6ff00165
codeaprendiz <codeaprendiz@gmail.com>
2023-12-02 09:40:36 +0400

    chore: update

--- home/codility/easy/taskset_easy_codility/task_001_binary_gap/binary_gap.go
+++ home/codility/easy/taskset_easy_codility/task_001_binary_gap/binary_gap.go
@@ -7,32 +7,14 @@ import (
 
 func Solution(N int) int {
 	// Convert N to binary string
-	binaryN := strconv.FormatInt(int64(N), 2)
-	fmt.Println(binaryN)
-
+	fmt.Println("Integer received : ", N)
+	fmt.Println("int64(N) : ", int64(N))
 	currentGap, maxGap := 0, 0
-	// Iterate through each character in the binary string
-	for _, bit := range binaryN {
-		fmt.Printf("currentGap : %v, maxGap : %v, bit : %v\n", currentGap, maxGap, bit)
-		fmt.Println(bit)
-		if bit == '1' {
-			fmt.Println("Bit is 1")
-			// Update maxGap if currentGap is larger
-			if currentGap > maxGap {
-				fmt.Println("currentGap > maxGap")
-				maxGap = currentGap
-			}
-			// Reset curentGap to 0 after finding bit 1
-			currentGap = 0
-		} else if bit == '0' {
-			// Only count gaps after the first one has been found
-			fmt.Println("currentGap >= 0 OR bit is 0")
-			currentGap += 1
-		} else {
-			fmt.Println("Default")
-		}
-	}
+	// http://golang.org/pkg/strconv/#FormatInt
+	BinaryN := strconv.FormatInt(int64(N), 2)
 
+	fmt.Println("BinaryN : ", BinaryN)
+	fmt.Printf("currentGap : %v, maxGap : %v\n", currentGap, maxGap)
 	return maxGap
 }
 
