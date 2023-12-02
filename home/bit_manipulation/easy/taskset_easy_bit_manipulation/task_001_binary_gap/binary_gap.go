26ba18b8aaa428991ea429aa0a7fab9fc616c7b6
codeaprendiz <codeaprendiz@gmail.com>
2023-12-02 12:05:57 +0400

    chore: update

--- home/bit_manipulation/easy/taskset_easy_bit_manipulation/task_001_binary_gap/binary_gap.go
+++ home/bit_manipulation/easy/taskset_easy_bit_manipulation/task_001_binary_gap/binary_gap.go
@@ -41,12 +41,8 @@ func main() {
 			package main
 	*/
 
-	// fmt.Println(Solution(6291457)) // returns 20
-	// fmt.Println(Solution(74901729))   // returns 4
-	// fmt.Println(Solution(805306373))  // returns 25
-	// fmt.Println(Solution(1073741825)) // returns 29
-	// fmt.Println(Solution(1376796946)) // returns 5
-	// fmt.Println(Solution(1610612737)) // returns 28
-	// fmt.Println(Solution(2147483647)) // returns 0
+	// fmt.Println(Solution(9))
+	// fmt.Println(Solution(7778742049))
+	// fmt.Println(Solution(12586269025))
 
 }
