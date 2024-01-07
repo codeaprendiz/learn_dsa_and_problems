84fb4c74fe01135f1edac8a844eaab6459222ec9
codeaprendiz <codeaprendiz@gmail.com>
2024-01-07 13:19:44 +0400

    chore: doubly linked list, delete node

--- home/list/easy/taskset_easy_list/task_004_doubly_linked_list__append_prepend_deleteWithValue_print/doubly_linked_list.go
+++ home/list/easy/taskset_easy_list/task_004_doubly_linked_list__append_prepend_deleteWithValue_print/doubly_linked_list.go
@@ -40,16 +40,46 @@ func (ll *LinkedList) appendNodeToListWithData(data int) {
 
 // To delete from the linkedlist
 func (ll *LinkedList) deleteNodeFromListWithData(data int) {
+	if ll.head == nil { // If the first Node is null
+		return
+	}
 
+	if ll.head.val == data {
+		ll.head = ll.head.next // head now points to the second Node, we have to delete the first Node
+		if ll.head != nil {    // what is the second node is Nil
+			ll.head.prev = nil // second node's prev pointer is no longer pointing to firstNode as it's deleted
+		}
+	}
+
+	// Find the node to delete
+	cur := ll.head
+	for cur != nil && cur.val != data {
+		cur = cur.next
+	}
+
+	// If node was not found
+	if cur == nil {
+		return
+	}
+
+	// Update the pointer to exclude the current node
+	if cur.next != nil {
+		cur.next.prev = cur.prev
+	}
+	if cur.prev != nil {
+		cur.prev.next = cur.next
+	}
 }
 
 // To display list in one line
 func (ll *LinkedList) displayList() {
 	cur := ll.head
+	fmt.Printf("\nList Details : ")
 	for cur != nil {
 		fmt.Printf(" %v ", cur.val)
 		cur = cur.next
 	}
+	fmt.Printf("\n")
 }
 
 func main() {
@@ -57,5 +87,7 @@ func main() {
 	ll.appendNodeToListWithData(1)
 	ll.appendNodeToListWithData(2)
 	ll.appendNodeToListWithData(3)
+	ll.displayList()
+	ll.deleteNodeFromListWithData(2)
 	ll.displayList()
 }
